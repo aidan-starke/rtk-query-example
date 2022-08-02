5070dea768e5307e8b0ebf34dd140a5cab0a081e
aidan-starke <starke.aidan@gmail.com>
2022-08-03 08:45:14 +1200

    Add `Bk` block

--- libs/components/Block.tsx
+++ libs/components/Block.tsx
@@ -21,17 +21,27 @@ export const Block: FC<BlockProps> = ({
 
 	return (
 		<div className="p-4 flex items-center border-b">
-			<div className="flex-1">
-				<p className="text-blue-600">{height}</p>
-				<p className="text-sm">{blockAge}</p>
+			<div className="flex flex-1 space-x-2">
+				<div className="bg-gray-300 h-12 w-12 flex items-center rounded">
+					<p className="text-center w-full">Bk</p>
+				</div>
+				<div>
+					<a
+						className="text-blue-600"
+						href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+					>
+						{height}
+					</a>
+					<p className="text-sm">{blockAge}</p>
+				</div>
 			</div>
 			<div>
 				<p>
-					Hash: <span className="font-mono text-sm text-blue-600">{hash}</span>
+					Hash: <span className="font-mono text-sm text-gray-500">{hash}</span>
 				</p>
 				<p>
 					Parent:{" "}
-					<span className="font-mono text-sm text-blue-600">{parentHash}</span>
+					<span className="font-mono text-sm text-gray-500">{parentHash}</span>
 				</p>
 			</div>
 		</div>
