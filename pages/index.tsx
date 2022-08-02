3c21756d7ed59a78ecb67a6f8c0a5113721eab75
aidan-starke <starke.aidan@gmail.com>
2022-08-02 12:13:15 +1200

    Display blocks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
+import { api } from "../api/generated";
+import JSONPretty from "react-json-pretty";
 
 const Home: NextPage = () => {
+	const { data } = api.endpoints.GetBlocks.useQuery();
+
 	return (
 		<div className="h-screen w-2/3 p-8 m-auto">
 			<h1 className="text-2xl font-mono">Blocks</h1>
-			<div className="border-2 rounded h-full"></div>
+			<div className="border-2 rounded h-full overflow-y-auto">
+				<JSONPretty id="json-blocks" data={data} />
+			</div>
 		</div>
 	);
 };
