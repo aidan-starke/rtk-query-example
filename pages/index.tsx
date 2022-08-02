129d05f4a33a03284520b1e725f7b9b984e728cd
aidan-starke <starke.aidan@gmail.com>
2022-08-02 13:31:55 +1200

    Poll blocks every 5s

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import type { NextPage } from "next";
 import { useGetBlocksQuery } from "../api/generated";
 import JSONPretty from "react-json-pretty";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
-	const { data, isFetching } = useGetBlocksQuery();
+	const { data, isFetching } = useGetBlocksQuery(undefined, {
+		pollingInterval: 5000,
+	});
+	const isFetched = useIsFetched(isFetching);
 
 	return (
 		<div className="h-screen w-2/3 p-8 m-auto">
 			<h1 className="text-2xl font-mono">Blocks</h1>
 			<div className="border-2 rounded h-full overflow-y-auto p-4">
-				{isFetching ? (
-					<p className="font-mono">Loading...</p>
-				) : (
+				{isFetched ? (
 					<JSONPretty id="json-blocks" data={data} />
+				) : (
+					<p className="font-mono">Loading...</p>
 				)}
 			</div>
 		</div>
@@ -20,3 +24,15 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+const useIsFetched = (isFetching: boolean) => {
+	const [isFetched, setIsFetched] = useState<boolean>(false);
+
+	useEffect(() => {
+		if (isFetched) return;
+
+		setIsFetched(!isFetching);
+	}, [isFetching]);
+
+	return isFetched;
+};
