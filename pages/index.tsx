3ce02fdf3fdb2a9941e1cdc361527cb8b7ab6be0
aidan-starke <starke.aidan@gmail.com>
2022-08-02 12:31:18 +1200

    Use `isFetching` from `useQuery` to show loading state

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,17 @@ import { api } from "../api/generated";
 import JSONPretty from "react-json-pretty";
 
 const Home: NextPage = () => {
-	const { data } = api.endpoints.GetBlocks.useQuery();
+	const { data, isFetching } = api.endpoints.GetBlocks.useQuery();
 
 	return (
 		<div className="h-screen w-2/3 p-8 m-auto">
 			<h1 className="text-2xl font-mono">Blocks</h1>
-			<div className="border-2 rounded h-full overflow-y-auto">
-				<JSONPretty id="json-blocks" data={data} />
+			<div className="border-2 rounded h-full overflow-y-auto p-4">
+				{isFetching ? (
+					<p className="font-mono">Loading...</p>
+				) : (
+					<JSONPretty id="json-blocks" data={data} />
+				)}
 			</div>
 		</div>
 	);
